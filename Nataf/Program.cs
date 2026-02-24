6fe005b54be104990f334a79f566d82ff48976be
atom-bowl <sachinshankarsachin@gmail.com>
2026-02-24 15:23:16 -0800

    Add project files.

---
+++ Nataf/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Tocarme");
+Console.WriteLine("This is a console game called Nataf.");   
